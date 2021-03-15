a93f6a726af752921b60706da473dbcd4df0edda
gabriel.carlos <gabriel.carlos@hotmart.com>
2021-03-15 00:03:36 -0300

    Solução com lib resilience4j

--- api-product/src/main/java/me/clementino/apiproduct/service/PriceServiceImpl.java
+++ api-product/src/main/java/me/clementino/apiproduct/service/PriceServiceImpl.java
@@ -1,5 +1,9 @@
 package me.clementino.apiproduct.service;
 
+import io.github.resilience4j.circuitbreaker.CircuitBreaker;
+import io.github.resilience4j.circuitbreaker.CircuitBreakerConfig;
+import io.github.resilience4j.circuitbreaker.CircuitBreakerRegistry;
+import io.vavr.control.Try;
 import lombok.extern.slf4j.Slf4j;
 import me.clementino.apiproduct.client.PriceServiceClient;
 import me.clementino.apiproduct.domain.entity.Product;
@@ -7,13 +11,29 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.math.BigDecimal;
+import java.time.Duration;
 import java.util.Optional;
+import java.util.function.Supplier;
 
 @Slf4j
 @Service
 public class PriceServiceImpl implements PriceService {
 
     private final PriceServiceClient priceServiceClient;
+
+    private static CircuitBreaker circuitBreaker;
+
+    static {
+        CircuitBreakerConfig circuitBreakerConfig = CircuitBreakerConfig.custom()
+                .failureRateThreshold(3)
+                .minimumNumberOfCalls(0)
+                .waitDurationInOpenState(Duration.ofSeconds(45))
+                .recordExceptions(Exception.class)
+                .build();
+
+        CircuitBreakerRegistry circuitBreakerRegistry = CircuitBreakerRegistry.of(circuitBreakerConfig);
+        circuitBreaker = circuitBreakerRegistry.circuitBreaker("getPrice");
+    }
 
     @Autowired
     public PriceServiceImpl(PriceServiceClient priceServiceClient) {
@@ -22,7 +42,13 @@ public class PriceServiceImpl implements PriceService {
 
     @Override
     public Optional<BigDecimal> getPriceByProduct(Product product) {
-        return Optional.ofNullable(priceServiceClient.fetchPriceByProductId(product.getId()));
+        Supplier<BigDecimal> tSupplier = CircuitBreaker.decorateSupplier(circuitBreaker, () -> priceServiceClient.fetchPriceByProductId(product.getId()));
+        Try<BigDecimal> recover = Try.of(tSupplier::get).recover(t -> handleException(t, product));
+        return Optional.ofNullable(recover.get());
+    }
+
+    private BigDecimal handleException(Throwable t,  Product product) {
+        log.info(String.format("The circuit breaker is OPEN. The service exception is %s. The product's price will be %s", t.getCause().getMessage(), product.getPrice()));
+        return product.getPrice();
     }
 }
-
